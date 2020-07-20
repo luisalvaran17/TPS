5dbb028cf9d9ca1b89668dcda81666787206851e
lclucumi <luz.lucumi@correounivalle.edu.co>
2020-07-20 11:45:19 -0500

    Update TPS_DB.sql

--- TPS_DB.sql
+++ TPS_DB.sql
@@ -18,9 +18,10 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de datos: `TPS_DB`
+--
 
-CREATE DATABASE TPS_DB;
-USE TPS_DB;
 
 -- --------------------------------------------------------
 
